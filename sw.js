658cf0164c5c8a0386bdd416219bf94d0fe5cd5e
gread66 <3112511778@qq.com>
2025-06-25 17:32:06 +0800

    Site updated: 2025-06-25 17:32:05

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1750843762054";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1750843919871";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
